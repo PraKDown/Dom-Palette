476531bffb2b9656200efb729e1d3f3a407bfd14
Vyacheslav Tkachev <dragbarxd@gmail.com>
2019-05-06 14:32:13 +0300

    refactor: code style fix

--- src/index.js
+++ src/index.js
@@ -1,109 +1,112 @@
 window.onload = function () {
-  let action = undefined;
-  let bucket = document.querySelector(".bucket");
-  let picker = document.querySelector(".picker");
-  let move = document.querySelector(".move");
-  let transform = document.querySelector(".transform");
-  let elems = document.querySelectorAll(".elem");
-  let body = document.querySelector("body");
-  let save = document.querySelector(".save");
-  let load = document.querySelector(".load");
-  let zind = 1;
+  let action;
+  const bucket = document.querySelector(".bucket");
+  const picker = document.querySelector(".picker");
+  const move = document.querySelector(".move");
+  const transform = document.querySelector(".transform");
+  const elems = document.querySelectorAll(".elem");
+  const body = document.querySelector("body");
+  const save = document.querySelector(".save");
+  const load = document.querySelector(".load");
+  const previos = document.querySelector(".prev");
+  const current = document.querySelector(".cur");
+  let zIndex = 1;
+  const LEFT_CANVAS_BORDER = 10;
+  const RIGHT_CANVAS_BORDER = 190;
   
-  function buck() {
-    action = 1;
+  function paintBucket() {
+    action = "paintBucket";
     body.style.cursor = "url('./palette/bucket.webp'), auto";
   }
 
-  function pick() {
-    action = 2;
+  function chooseColor() {
+    action = "chooseColor";
     body.style.cursor = "url('./palette/color-picker.webp'), auto";
   }
 
-  function mo() {
-    action = 3;
+  function moveAction() {
+    action = "moveAction";
     body.style.cursor = "url('./palette/move.webp'), auto";
   }
 
-  function trans() {
-    action = 4;
+  function transformation() {
+    action = "transformation";
     body.style.cursor = "url('./palette/transform.webp'), auto";
   }
 
-  function lo() {
-    for (let i = 0; i < elems.length; i++) {
-      let m = "check" + i;
-      let styl = JSON.parse(localStorage.getItem(m));
-      for (let key in styl) {
-        elems[i].style[key] = styl[key];
+  function loadChanges() {
+    elems.forEach((elem, index) => {
+      let keyOfStorage = "check" + index;
+      let style = JSON.parse(localStorage.getItem(keyOfStorage));
+      for (let key in style) {
+        elem.style[key] = style[key];
       }
-    }
+    })
     body.style.cursor = "auto";
   }
 
-  function sa() {
-    for (let i = 0; i < elems.length; i++) {
-      let m = "check" + i;
-      localStorage.removeItem(m);
-      localStorage.setItem(m, JSON.stringify(elems[i].style));  
-    }
+  function saveChanges() {
+    elems.forEach((elem, index) => {
+      let keyOfStorage = "check" + index;
+      localStorage.removeItem(keyOfStorage);
+      localStorage.setItem(keyOfStorage, JSON.stringify(elem.style));  
+    })
     body.style.cursor = "auto";
   }
 
   document.addEventListener("keydown", event => {
     switch (event.keyCode) {
       case 80:
-        buck();
+        paintBucket();
         break;
       case 67:
-        pick();
+        chooseColor();
         break;
       case 77:
-        mo();
+        moveAction();
         break;
       case 84:
-        trans();
+        transformation();
         break;
       case 83:
-        sa();
+        saveChanges();
         break;
       case 76:
-        lo();
+        loadChanges();
         break;
       default:
         break;
     }
   });
 
-  bucket.addEventListener("click", buck);
+  bucket.addEventListener("click", paintBucket);
 
-  picker.addEventListener("click", pick);
+  picker.addEventListener("click", chooseColor);
 
-  move.addEventListener("click", mo);
+  move.addEventListener("click", moveAction);
 
-  transform.addEventListener("click", trans);
+  transform.addEventListener("click", transformation);
 
-  for (let i = 0 ; i < elems.length; i++) {
-    let el = elems[i];
-    el.addEventListener('click' , function () {
+  elems.forEach((elem) => {
+    elem.addEventListener('click' , function () {
       switch (action) {
-        case 1:
-          el.style.backgroundColor = document.querySelectorAll(".car")[0].style.backgroundColor;
+        case "paintBucket":
+          elem.style.backgroundColor = current.style.backgroundColor;
           break;
-        case 4:
-          if (el.style.borderRadius === "100%") el.style.borderRadius = "";
-          else el.style.borderRadius = "100%";
+        case "transformation":
+          if (elem.style.borderRadius === "100%") elem.style.borderRadius = "";
+          else elem.style.borderRadius = "100%";
           break;
         default:
           break;
       }
     } ) ; 
-  }
+  })
 
   body.addEventListener("click", (event) => {
-    if (action === 2) {
-      document.querySelectorAll(".prev")[0].style.backgroundColor = document.querySelectorAll(".car")[0].style.backgroundColor
-      document.querySelectorAll(".car")[0].style.backgroundColor = window.getComputedStyle(event.target).backgroundColor
+    if (action === "chooseColor") {
+      previos.style.backgroundColor = current.style.backgroundColor;
+      current.style.backgroundColor = window.getComputedStyle(event.target).backgroundColor;
     } 
   });
 
@@ -112,43 +115,42 @@ window.onload = function () {
     elem.style.top = e.pageY - elem.offsetHeight / 2 + 'px';
   }
 
-
-  for (let i = 0 ; i < elems.length; i++) {
-    elems[i].onmousedown = function(e) {
-      if (action === 3) {
-        let x1 = Number(elems[i].style.left.split("px")[0]);
-        let y1 = Number(elems[i].style.top.split("px")[0]);
-        moveAt(e, elems[i]);
-        elems[i].style.zIndex = ++zind; 
+  elems.forEach((elem, index) => {
+    elem.onmousedown = function(e) {
+      if (action === "moveAction") {
+        let x1 = Number(elem.style.left.split("px")[0]);
+        let y1 = Number(elem.style.top.split("px")[0]);
+        moveAt(e, elem);
+        elem.style.zIndex = ++zIndex; 
         document.onmousemove = function(e) {
-          moveAt(e, elems[i]);
+          moveAt(e, elem);
         }
-        elems[i].onmouseup = function(e) {
-          for (let j = 0; j < elems.length; j++) {
-            if (j != i) {
-              let x2 = Number(elems[j].style.left.split("px")[0]);
-              let y2 = Number(elems[j].style.top.split("px")[0]);
+        elem.onmouseup = function(e) {
+          elems.forEach((elem2, index2) => {
+            if (index != index2) {
+              let x2 = +elem2.style.left.split("px")[0];
+              let y2 = +elem2.style.top.split("px")[0];
+              const IS_X_INSIDE_CANVAS = e.pageX > x2 + LEFT_CANVAS_BORDER &&  e.pageX < x2 + RIGHT_CANVAS_BORDER;
+              const IS_Y_INSIDE_CANVAS = e.pageY > y2 + LEFT_CANVAS_BORDER && e.pageY < y2 + RIGHT_CANVAS_BORDER;
 
-              if (e.pageX > x2 + 10 && e.pageX < x2 + 190 && e.pageY > y2 + 10 && e.pageY < y2 + 190) {
-                elems[j].style.left = x1 + "px";
-                elems[j].style.top = y1 + "px";
-                break;
+              if (IS_X_INSIDE_CANVAS && IS_Y_INSIDE_CANVAS) {
+                elem2.style.left = x1 + "px";
+                elem2.style.top = y1 + "px";
               }
             }
-          }
-
+          })
           document.onmousemove = null;
-          elems[i].onmouseup = null;
+          elem.onmouseup = null;
         }
       }
     }
-    elems[i].ondragstart = function() {
+    elem.ondragstart = function() {
       return false;
     }; 
-  }
+  })
 
-  save.addEventListener("click", sa)
+  save.addEventListener("click", saveChanges)
 
-  load.addEventListener("click", lo)
+  load.addEventListener("click", loadChanges)
 
 }
